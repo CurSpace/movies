5acb32305e8346b68527278e5853d4af83ab2dd3
CurSpace <callpradip96@gmail.com>
2023-08-08 11:22:09 -0500

    one controller done

---
+++ src/test/java/com/display/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.display.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
