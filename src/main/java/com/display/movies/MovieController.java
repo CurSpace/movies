f1e100f71eb97ee540b48612ee015876e9c3966d
CurSpace <callpradip96@gmail.com>
2023-08-08 22:50:33 -0500

    movie controller ready!

--- src/main/java/com/display/movies/MovieController.java
+++ src/main/java/com/display/movies/MovieController.java
@@ -1,16 +1,29 @@
 package com.display.movies;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Mov>> getAllMovies(){
+        return new ResponseEntity<List<Mov>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public  ResponseEntity<Optional<Mov>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Mov>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 }
